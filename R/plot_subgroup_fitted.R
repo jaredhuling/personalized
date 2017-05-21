8f03fd85fa87aba64b10c29d5823bf254beb9272
jaredhuling <huling@wisc.edu>
2017-05-21 16:54:05 -0500

    added plot function for fit.subgrp objects

--- R/plot_subgroup_fitted.R
+++ R/plot_subgroup_fitted.R
@@ -1,21 +1,16 @@
-#' Plotting validation results for fitted subgroup identification models
+#' Plotting results for fitted subgroup identification models
 #'
-#' @description Plots validation results for estimated subgroup treatment effects
+#' @description Plots results for estimated subgroup treatment effects
 #'
-#' @param x fitted object returned by \code{validate.subgrp()} function
+#' @param x fitted object returned by \code{fit.subgrp()} function
 #' @param type type of plot. \code{"density"} results in a density plot for the results
-#' across all iterations of either the bootstrap or training/test re-fitting. For the latter
-#' case the test results will be plotted. \code{"boxplot"} results in boxplots across all iterations of either
-#' the bootstrap or training/test re-fitting. For the latter
-#' case the test results will be plotted. \code{"interaction"} creates an
+#' across all patients. \code{"boxplot"} results in boxplots across observations. \code{"interaction"} creates an
 #' interaction plot for the different subgroups (crossing lines here means a meaningful subgroup)
 #' @param avg.line boolean value of whether or not to plot a line for the average
 #' value in addition to the density (only valid for \code{type = "density"})
 #' @param ... not used
-#' @seealso \code{\link[personalized]{validate.subgrp}} for function which creates validation results
-#' and \code{\link[personalized]{fit.subgrp}} for function which fits subgroup identification models.
+#' @seealso \code{\link[personalized]{fit.subgrp}} for function which fits subgroup identification models.
 #' @rdname plot
-#' @import ggplot2
 #'
 #' @examples
 #' library(personalized)
@@ -61,46 +56,41 @@
 #'
 #' subgrp.model$subgroup.trt.effects
 #'
-#' x.test <- matrix(rnorm(10 * n.obs * n.vars, sd = 3), 10 * n.obs, n.vars)
-#'
-#'
-#' # simulate non-randomized treatment
-#' xbetat.test   <- 0.5 + 0.5 * x.test[,21] - 0.5 * x.test[,41]
-#' trt.prob.test <- exp(xbetat.test) / (1 + exp(xbetat.test))
-#' trt01.test    <- rbinom(10 * n.obs, 1, prob = trt.prob.test)
-#'
-#' trt.test      <- 2 * trt01.test - 1
-#'
-#' # simulate response
-#' delta.test <- 2 * (0.5 + x.test[,2] - x.test[,3] - x.test[,11] + x.test[,1] * x.test[,12])
-#' xbeta.test <- x.test[,1] + x.test[,11] - 2 * x.test[,12]^2 + x.test[,13]
-#' xbeta.test <- xbeta.test + delta.test * trt.test
-#'
-#' y.test <- drop(xbeta.test) + rnorm(10 * n.obs, sd = 2)
-#'
-#' valmod <- validate.subgrp(subgrp.model, B = 10,
-#'                           method = "training_test",
-#'                           train.fraction = 0.75)
-#' valmod$avg.results
-#'
-#' plot(valmod)
+#' plot(subgrp.model)
 #' @export
-plot.subgroup_validated <- function(x,
-                                    type = c("density", "boxplot", "interaction"),
-                                    avg.line = TRUE,
-                                    ...)
+plot.subgroup_fitted <- function(x,
+                                 type = c("density", "boxplot", "interaction"),
+                                 avg.line = TRUE,
+                                 ...)
 {
     type <- match.arg(type)
 
     avg.line <- as.logical(avg.line[1])
 
-    boot.res <- x$boot.results$avg.outcomes
-    avg.res  <- x$avg.results
-    B <- dim(boot.res)[1]
+    avg.res  <- x$subgroup.trt.effects
 
-    res.2.plot <- array(NA, dim = c(B * 4, 3))
-    colnames(res.2.plot) <- c("Recommended", "Received", "Value")
-    res.2.plot <- data.frame(res.2.plot)
+    if (type != "interaction")
+    {
+        if (is.null(x$call)) stop("retcall argument must be set to TRUE for fitted model object")
+
+        benefit.scores <- x$benefit.scores
+        B <- NROW(benefit.scores)
+
+        res.2.plot <- array(NA, dim = c(B, 3))
+        colnames(res.2.plot) <- c("Recommended", "Received", "Value")
+        res.2.plot <- data.frame(res.2.plot)
+
+        cutpoint <- x$call$cutpoint
+        lb       <- x$call$larger.outcome.better
+
+        trt.rec  <- if (lb) {1 * (benefit.scores > cutpoint)} else
+            1 * (benefit.scores < cutpoint)
+
+        res.2.plot[, 1] <- ifelse(trt.rec == 1, "Recommended Trt", "Recommended Ctrl")
+        res.2.plot[, 2] <- ifelse(x$call$trt == 1, "Received Trt", "Received Ctrl")
+        res.2.plot[, 3] <- x$call$y
+    }
+
 
     avg.res.2.plot <- data.frame(Recommended = c("Recommended Trt", "Recommended Trt",
                                                  "Recommended Ctrl", "Recommended Ctrl"),
@@ -110,32 +100,24 @@ plot.subgroup_validated <- function(x,
 
     Recommended <- Received <- Value <- NULL
 
-    for (b in 1:B)
-    {
-        cur.idx <- c(((b - 1) * 4 + 1):(b * 4))
-        res.2.plot[cur.idx, 1] <- c("Recommended Trt", "Recommended Trt",
-                                    "Recommended Ctrl", "Recommended Ctrl")
-        res.2.plot[cur.idx, 2] <- c("Received Trt", "Received Ctrl",
-                                    "Received Trt", "Received Ctrl")
-        res.2.plot[cur.idx, 3] <- as.vector(boot.res[b,,])
-    }
+
 
     if (type == "density")
     {
         pl.obj <- ggplot(res.2.plot,
                          aes(x = Value, fill = Received)) +
-                      geom_density(alpha = 0.65) +
-                      geom_rug(aes(colour = Received), alpha = 0.85) +
-                      coord_flip() +
-                      facet_grid( ~ Recommended) +
-                      theme(legend.position = "bottom")
+            geom_density(alpha = 0.65) +
+            geom_rug(aes(colour = Received), alpha = 0.85) +
+            coord_flip() +
+            facet_grid( ~ Recommended) +
+            theme(legend.position = "bottom")
         if (avg.line)
         {
             pl.obj <- pl.obj + geom_vline(data = avg.res.2.plot,
                                           aes(xintercept = Value),
                                           size = 1.25) +
-                               geom_vline(data = avg.res.2.plot,
-                                          aes(xintercept = Value, colour = Received))
+                geom_vline(data = avg.res.2.plot,
+                           aes(xintercept = Value, colour = Received))
         }
     } else if (type == "boxplot")
     {
