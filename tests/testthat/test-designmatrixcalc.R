729b616fa58c33071a6cb377d0d4fc843a9fcfaa
jaredhuling <huling.7@osu.edu>
2017-09-26 22:37:55 -0400

    more tests added

---
+++ tests/testthat/test-designmatrixcalc.R
@@ -0,0 +1,46 @@
+
+context("design matrix checking")
+
+test_that("test design matrix for binary trt works - weighting", {
+
+    x <- matrix(1:8, ncol = 2)
+    y <- 1:4
+    trt <- c(rep(1, 2), rep(0, 2))
+    pi.x <- seq(0.2, 0.8, length.out = 4)
+
+    xtilde <- create.design.matrix.binary.trt(x, pi.x, trt, "weighting")
+
+    expect_equivalent(xtilde, (2 * trt - 1)  * cbind(1, x) )
+})
+
+test_that("test design matrix for binary trt works - a learning", {
+
+    x <- matrix(1:8, ncol = 2)
+    y <- 1:4
+    trt <- c(rep(1, 2), rep(0, 2))
+    pi.x <- seq(0.2, 0.8, length.out = 4)
+
+    xtilde <- create.design.matrix.binary.trt(x, pi.x, trt, "a_learning")
+
+    expect_equivalent(xtilde, (1 * (trt != 0) - pi.x)   * cbind(1, x) )
+})
+
+test_that("test weights for binary trt works - weighting", {
+
+    trt <- c(rep(1, 2), rep(0, 2))
+    pi.x <- seq(0.2, 0.8, length.out = 4)
+
+    wts <- create.weights.binary.trt(pi.x = pi.x, trt = trt, "weighting")
+
+    expect_equivalent(wts, 1 / (pi.x * (trt == 1) + (1 - pi.x) * (trt == 0)) )
+})
+
+test_that("test weights for binary trt works - a_learning", {
+
+    trt <- c(rep(1, 2), rep(0, 2))
+    pi.x <- seq(0.2, 0.8, length.out = 4)
+
+    wts <- create.weights.binary.trt(pi.x = pi.x, trt = trt, "a_learning")
+
+    expect_equivalent(wts, rep(1, length(trt)) )
+})
