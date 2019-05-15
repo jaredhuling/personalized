5f5faceb60a89b6cb459a2cd073cf48c9f1aacc8
jaredhuling <huling.7@osu.edu>
2019-05-15 16:51:24 -0400

    remove bug

--- R/weighted_svm.R
+++ R/weighted_svm.R
@@ -220,7 +220,6 @@ weighted.ksvm <- function(y,
                                                       r = 0,
                                                       l = rep(0, nsub),
                                                       u = drop(C[l] * weights[-which.test]),
-                                                      eps = epsil,
                                                       ...), silent = TRUE)
 
                 if (exists("fitsub"))
@@ -250,7 +249,6 @@ weighted.ksvm <- function(y,
                             r = 0,
                             l = rep(0, n),
                             u = drop(best.C * weights),
-                            eps = epsil,
                             ...)
 
     if (fit@how != "converged")
